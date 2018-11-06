5ee550563baa3163c12ab05cdf8761bfdaa0a1db
Lorna Harwood <lornapeepwreckers@gmail.com>
2018-11-06 15:55:23 +0000

    add new test and method for deduct

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -7,7 +7,7 @@ describe Oystercard do
 
   it { expect(subject.balance).to eq 0 }
 
-  describe '#top_up' do
+    describe '#top_up' do
     it { is_expected.to respond_to(:top_up).with(1).argument }
 
     it 'adding money to card' do
@@ -15,13 +15,22 @@ describe Oystercard do
     end
   end
 
-  describe '#LIMIT'
-  it 'throw error whem limit is exceeded' do
-    maximum_limit = Oystercard::LIMIT
-    subject.top_up(maximum_limit)
-    expect{ subject.top_up 1 }.to raise_error 'Max limit is £#{LIMIT}'
-
+  describe '#LIMIT' do
+    it 'throw error whem limit is exceeded' do
+      maximum_limit = Oystercard::LIMIT
+      subject.top_up(maximum_limit)
+      expect{ subject.top_up 1 }.to raise_error 'Max limit is £#{LIMIT}'
+    end
   end
 
+  describe '#deduct' do
+
+    it { is_expected.to respond_to(:deduct).with(1).argument }
+
+    it "to deduct money from the card" do
+      subject.top_up(20)
+      expect{ subject.deduct 3 }.to change {subject.balance }.by -3
+    end
+  end
 
 end
